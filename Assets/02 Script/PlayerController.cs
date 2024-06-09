383d4825070a9055843a91491221868fc97c369a
오하늘 <dhgksmfdl@naver.com>
2024-06-09 17:51:15 +0900

    3. 이동변경, 애니메이션 미완
    애니메이션 개이상함

--- Assets/02 Script/PlayerController.cs
+++ Assets/02 Script/PlayerController.cs
@@ -2,32 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum SIDE { Left, Center, Right }
+
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 10;        // 캐릭터가 달리는 속도
-    public float leftRightSpeed = 7;    // 양 옆으로 움직이는 속도
-    //public 쓸까 private 쓸까 
+    public SIDE c_Side = SIDE.Center;
+    float NewXPos = 0f;
+    public bool SwipeLeft;
+    public bool SwipeRight;
+    public float XValue = 2.5f;
+    private CharacterController m_char;
+    private Animator m_Animator;
+    private float x;
+    public float SpeedDodge;
+
+    private void Start()
+    {
+        m_char = GetComponent<CharacterController>();
+        m_Animator = GetComponent<Animator>();
+        transform.position = Vector3.zero;
+    }
 
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        SwipeLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A); // 왼쪽 이동은 방향키랑 A키
+        SwipeRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D); // 오른쪽 이동은 방향키랑 D키
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) // 왼쪽 이동은 방향키랑 A키
+        if (SwipeLeft)
         {
-            if (this.gameObject.transform.position.x > Boundary.leftside)
+            if (c_Side == SIDE.Center)
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                NewXPos = -XValue;
+                c_Side = SIDE.Left;
+                m_Animator.Play("DodgeLeft");
+            }
+            else if (c_Side == SIDE.Right)
+            {
+                NewXPos = 0;
+                c_Side = SIDE.Center;
+                m_Animator.Play("DodgeLeft");
             }
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) // 오른쪽 이동은 방향키랑 D키
+        else if (SwipeRight)
         {
-            if (this.gameObject.transform.position.x < Boundary.rightside)
+            if (c_Side == SIDE.Center)
             {
-                transform.Translate(Vector3.right * Time.deltaTime * leftRightSpeed);
+                NewXPos = XValue;
+                c_Side = SIDE.Right;
+                m_Animator.Play("DodgeRight");
+            }
+            else if (c_Side == SIDE.Left)
+            {
+                NewXPos = 0;
+                c_Side = SIDE.Center;
+                m_Animator.Play("DodgeRight");
             }
         }
+        x = Mathf.Lerp(x, NewXPos, Time.deltaTime * 10f * SpeedDodge);
+        m_char.Move((x - transform.position.x) * Vector3.right);
+
     }
 }
