221453e6ba3240e509753fc5257d9fd78a56692d
honnnney <tpgjs1023@gmail.com>
2024-06-14 18:27:34 +0900

    자석 아이템, 파츠 스폰
    몸, 팔, 머리 파츠 스폰하는 Spawn 스크립트.
    자석 아이템 생성하는 ItemSpawn 스크립트

--- Assets/02 Script/Magnet.cs
+++ Assets/02 Script/Magnet.cs
@@ -4,45 +4,51 @@ using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
-    public GameObject magnetPrefab; //자석 아이템 프리팹
-    public float spawnInterval = 5f; //아이템 생성 간격 기본 5초
     //public ParticleSystem magnetEffect; // 파티클 효과
-    //public float magnetRange = 2.7f; // 자석 효과의 범위
-    //public bool isMagnetActive = false;
+    //public float magnetRange = 2.7f;    //자석 효과 범위
+    //public GameObject magnetPrefab; //자석 아이템 프리팹
+    //public float spawnInterval = 5f; //아이템 생성 간격 기본 5초
+
+    public float magnetRange = 2.7f;
+    public float magnetSpeed = 5f;
+    public bool magnetActive = false;
+
+    public void ActivateMagnet()
+    {
+        magnetActive = true;
+    }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("SpawnMagnet", spawnInterval, spawnInterval);
-    }
+    //void Start()
+    //{
+    //    InvokeRepeating("SpawnMagnet", spawnInterval, spawnInterval);
+    //}
 
-    void SpawnMagnet()
-    {
-        // 현재 위치에서 자석 아이템 생성
-        Instantiate(magnetPrefab, transform.position, transform.rotation);
-    }
+    //void SpawnMagnet()
+    //{
+    //    // 현재 위치에서 자석 아이템 생성
+    //    Instantiate(magnetPrefab, transform.position, transform.rotation);
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (magnetActive)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, magnetRange);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.gameObject.CompareTag("Part"))
+                {
+                    hitCollider.gameObject.transform.position = Vector3.Lerp(
+                        hitCollider.gameObject.transform.position,
+                        transform.position,
+                        Time.deltaTime * magnetSpeed
+                    );
+                }
+            }
+        }
     }
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("Magnet"))
-    //    {
-    //        Destroy(other.gameObject);
-    //        isMagnetActive = true;
-    //        magnetEffect.Play(); // 파티클 효과 재생
-    //        StartCoroutine(MagnetEffect());
-    //    }
-    //}
-
-    //IEnumerator MagnetEffect()
-    //{
-    //    yield return new WaitForSeconds(10); // 자석 효과 지속 시간
-    //    isMagnetActive = false;
-    //    magnetEffect.Stop(); // 파티클 효과 중지
-    //}
+   
 }
--- Assets/02 Script/PlayerScript.cs
+++ Assets/02 Script/PlayerScript.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    //우리 좀비 오브젝트로 
-    public GameObject legs;
-    public GameObject body;
-    public GameObject arms;
-    public GameObject head;
-
-    private int partsCollected = 0; // 수집한 파츠 개수
-
+   
     //자석 효과
     public ParticleSystem magnetEffect; // 파티클 효과
     public float magnetRange = 2.7f;    //자석 효과 범위
@@ -20,11 +13,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 시작할 때는 모든 부위가 숨겨져 있음
-        legs.SetActive(false);
-        body.SetActive(false);
-        arms.SetActive(false);
-        head.SetActive(false);
+       
     }
 
     // Update is called once per frame
@@ -35,30 +24,7 @@ public class PlayerScript : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Part"))
-        {
-            Destroy(other.gameObject);
-            partsCollected++;
-
-            // 파츠 수에 따라 부위를 보이게 함, 예시로 20,40,60 넣음
-            if (partsCollected == 0)
-            {
-                legs.SetActive(true);
-            }
-            else if (partsCollected == 20)
-            {
-                body.SetActive(true);
-            }
-            else if (partsCollected == 40)
-            {
-                arms.SetActive(true);
-            }
-            else if (partsCollected == 60)
-            {
-                head.SetActive(true);
-            }
-        }
-
+        
         if (other.gameObject.CompareTag("Magnet"))
         {
             Destroy(other.gameObject); // 아이템 제거
